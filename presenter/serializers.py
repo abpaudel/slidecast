74fa0dc1517a351094225e5c78e873f00c4c916e
Abhishek Paudel <myheartswift@gmail.com>
2017-07-07 22:32:55 +0545

    added files and api working

---
+++ presenter/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Slide
+
+class SlideSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		model = Slide
+		fields = ('numberofslides', 'currentslide')
+
